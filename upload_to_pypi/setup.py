26c5fbcc85067cd09baf2ce43e5afadc65740122
dg1223 <alavi1223@hotmail.com>
2024-01-09 01:56:12 -0500

    chore: updated package version to 0.2

--- upload_to_pypi/setup.py
+++ upload_to_pypi/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 long_description = "This package contains code to perform basic mathematical operations on Gaussian and Binomial distributions. Operations include finding the mean, standard deviation (of both sample and population), plotting histograms for Gaussian distribution and plotting frequncy bar charts for Binomial distributions."
 
 setup(name='dg_probability',
-      version='0.1',
+      version='0.2',
       description='Gaussian and Binomial distributions',
       long_description=long_description,
       long_description_content_type="text"

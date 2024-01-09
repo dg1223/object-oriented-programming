42be275d9412d5297db250bae0b165cf4c132305
dg1223 <alavi1223@hotmail.com>
2024-01-09 01:55:24 -0500

    docs: added long description to setup file

--- upload_to_pypi/setup.py
+++ upload_to_pypi/setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
 
+long_description = "This package contains code to perform basic mathematical operations on Gaussian and Binomial distributions. Operations include finding the mean, standard deviation (of both sample and population), plotting histograms for Gaussian distribution and plotting frequncy bar charts for Binomial distributions."
+
 setup(name='dg_probability',
       version='0.1',
       description='Gaussian and Binomial distributions',
+      long_description=long_description,
+      long_description_content_type="text"
       packages=['dg_probability'],
       author="Shamir Alavi",
       author_email="dummy@e.mail",

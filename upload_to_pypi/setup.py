b3ec91ea04957b8ad7a3d2d13ba6dc2580aa9edc
dg1223 <alavi1223@hotmail.com>
2024-01-09 03:03:01 -0500

    chores: updated package version to 0.7

--- upload_to_pypi/setup.py
+++ upload_to_pypi/setup.py
@@ -6,7 +6,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(name='dg_probability',
-      version='0.6',
+      version='0.7',
       description='Gaussian and Binomial distributions',
       long_description=long_description,
       long_description_content_type="text/markdown",
